9e524f97dd44795d74845f21c7bfc2a83f727f5c
Ben <bensaine09@gmail.com>
2022-10-04 16:55:41 -0400

    react boilerplate

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h2>WOOHOO! YOU DID IT!</h2><p>Send '<b>🧸🫣🚣</b>' in Discord if you got this far! (iykyk)</p>
+        <small>
+          Edit <code>src/App.js</code> and save to reload.
+        </small>
+      </header>
+    </div>
+  );
+}
+
+export default App;
